7aba7962c6758b6897f14a8b33df67c3760d05e1
Thalleen <144317300+Thalleen@users.noreply.github.com>
2024-11-18 23:38:32 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const auth = (req,res,next)=>{
     const token = req.header("x-auth-token");
 
-    // Check for token
+    // Check for token  
     if(!token){
         return res.send(401).json({msg:"No token, authorization failed"});
     }
