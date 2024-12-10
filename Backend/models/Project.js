893de1cba7c1be0315e04f9b614ce4d3364c29d5
Thalleen <144317300+Thalleen@users.noreply.github.com>
2024-12-10 22:50:52 +0530

    Update Project.js

--- Backend/models/Project.js
+++ Backend/models/Project.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
-const projectSchema = new Schema({
+const projectSchema = new Schema({ 
     name: {type:String, required:true},
     description: {type:String},
     status: {type:String,enum:["Not Started","In Progress","Completed"],default:"Not Started"},
