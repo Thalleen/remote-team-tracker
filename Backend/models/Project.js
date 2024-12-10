03567faea50eeca1120b341ef40395d4248b17de
Thalleen <144317300+Thalleen@users.noreply.github.com>
2024-12-10 22:51:11 +0530

    Update Project.js

--- Backend/models/Project.js
+++ Backend/models/Project.js
@@ -6,7 +6,7 @@ const projectSchema = new Schema({
     description: {type:String},
     status: {type:String,enum:["Not Started","In Progress","Completed"],default:"Not Started"},
     owner: {type: mongoose.Schema.Types.ObjectId,ref:"User",required:true}, // User who created the project
-    teamMembers: [{type: mongoose.Schema.Types.ObjectId,ref:"User"}],
+    teamMembers: [{type: mongoose.Schema.Types.ObjectId,ref:"User"}], 
     createdAt: {type:Date,default:Date.now}
 });
 
