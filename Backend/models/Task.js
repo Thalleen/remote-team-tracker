b04c56686e6965d0a95a7a2919f3469e5a484541
Thalleen <144317300+Thalleen@users.noreply.github.com>
2024-12-11 22:25:15 +0530

    Update Task.js

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -8,7 +8,7 @@ const taskSchema = new Schema({
     assignedTo: {type: mongoose.Schema.Types.ObjectId, ref:"User"},
     status: {type:String, enum:["To Do","In Progress","Completed"],default:"To Do"},
     priority: {type:String, enum:["Low","Medium","High"],default:"Medium"},
-    duedate: {type:Date},
+    duedate: {type:Date}, 
     createdAt: {type:Date,default:Date.now}
  
 });
